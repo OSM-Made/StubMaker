44df747b01d247ef89e98bfbf91449b016df75bb
Greg <Greg@hackingadventures.ca>
2023-02-25 09:58:59 -0700

    Initial Commit

---
+++ Libs/build.bat
@@ -0,0 +1,11 @@
+set WorkingDir=%~dp0
+
+for /D %%G in ("%WorkingDir%*") do (
+	echo Entering %%G
+	for %%f in (%%G\*.def) do (
+		echo Building %%~nf
+		echo lib %%~nG
+		makeStub.bat %%~nG %%~nf %%G
+	)
+)
+pause
---
+++ Libs/makeStub.bat
@@ -0,0 +1,16 @@
+set LibraryName=%1
+set StubName=%2
+set WorkingDir=%3
+set OutDir=%4
+set StubDir=%WorkingDir%\%StubName%
+set SDKPath=C:\Program Files (x86)\SCE\ORBIS SDKs\8.000\host_tools\bin\
+
+"C:\Python27\python.exe" "%~dp0\emd_maker.py" %StubDir%.def
+
+"%SDKPath%orbis-clang.exe" -Wall -D__LIB__ -fno-builtin -O2 -Wno-return-type -c %StubDir%.c -o %StubDir%.c.o
+"%SDKPath%orbis-clang.exe" -Wall -D__LIB__ -D__ASM__ -c %StubDir%.S -o %StubDir%.S.o
+"%SDKPath%orbis-ld.exe" --oformat=prx --prx-stub-output-dir=%WorkingDir%\..\..\ --Map=%StubDir%.map -o %LibraryName%.sprx %StubDir%.c.o %StubDir%.S.o %StubDir%.emd
+
+Rem Clean up.
+del %WorkingDir%\..\%LibraryName%.sprx
+for /R %WorkingDir% %%f in (*) do (if not "%%~xf"==".def" del "%%~f")
