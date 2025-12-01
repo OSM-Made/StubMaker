e43a5d7ea73ce558e78c18f53eaadfa3cecc2bfe
Prefixmodz <166675297+Prefixmodz@users.noreply.github.com>
2025-12-01 00:16:24 +0000

    Correct the size of SceHidDeviceInfo

--- Include/Defs/HidControlDefs.h
+++ Include/Defs/HidControlDefs.h
@@ -15,12 +15,14 @@ extern "C" {
         int level; // 0x04
     };
 
+#pragma pack(push, 1)
     struct SceHidDeviceInfo // Unfinished
     {
         int _0x00; // type ?
         int deviceId; // 0x04
-        char pad_0x08[0x1F8]; // memset(SceHidDeviceInfo, 0, 0x202); in SysCore 0x05BC40
+        char pad_0x08[0x1FA]; // memset(SceHidDeviceInfo, 0, 0x202); in SysCore 0x05BC40
     };
+#pragma pack(pop)
 
 #ifdef __cplusplus
 }
