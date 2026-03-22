6e3abbc93b43e48992eb354b5585bfa877105e28
Prefixmodz <166675297+Prefixmodz@users.noreply.github.com>
2026-03-22 21:37:11 +0000

    Update Notification API
    Updated Notification API with osm's later research at https://github.com/OSM-Made/PS4-Notify

--- Include/KernelExt.h
+++ Include/KernelExt.h
@@ -7,7 +7,7 @@
 extern "C" {
 #endif
 
-	int sceKernelSendNotificationRequest(int device, void* req, size_t size, int blocking);
+	int sceKernelSendNotificationRequest(int api, void* buffer, size_t size, bool blocking);
 	int sceKernelGetAppInfo(int pid, SceAppInfo* info);
 	int sceKernelSetAppInfo(int pid, SceAppInfo* info);
 	int sceKernelSetProcessName(const char* name);
